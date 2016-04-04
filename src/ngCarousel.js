37a7a2e70bbe9d58bf4211505a6079d0086a8e15
Parminder Singh <paramsinghvc@gmail.com>
2016-04-04 14:02:23 +0530

    Thumbs Work

--- src/ngCarousel.js
+++ src/ngCarousel.js
@@ -20,7 +20,7 @@
 						$ul = $el.find('.carouselWrapper ul');
 						$carouselWrapper = $el.find('.carouselWrapper');
 						$thumbsWrapper = $el.find('.thumbsWrapper');
-                        $mainWrapper = $el.find('.mainWrapper');
+						$mainWrapper = $el.find('.mainWrapper');
 
 						$carouselWrapper.css({
 							width: $attrs.width + 'px',
@@ -32,6 +32,7 @@
 						});
 
 						return function($scope, $el, $attrs) {
+							$scope.activeIndex = 0;
 							$timeout(function() {
 								$el.find('.carouselWrapper ul').append($compile('<li ng-repeat="image in images"><img ng-src="{{image.src}}" alt="{{image.alt}}" width="' + $attrs.width + ' height="' + $attrs.height + '"></li>')($scope));
 
@@ -51,7 +52,7 @@
 								}
 
 								if ($scope.options.showThumbs) {
-									$el.find('.thumbsWrapper ul').append($compile('<li ng-repeat="image in images"><img ng-src="{{image.src}}" alt="{{image.alt}}"></li>')($scope));
+									$el.find('.thumbsWrapper ul').append($compile('<li ng-repeat="image in images" ng-class="{\'active\' : getActiveClass($index)}" ng-click="navigateThumbPic($index)"><img ng-src="{{image.src}}" alt="{{image.alt}}"></li>')($scope));
 								}
 
 								function rotateCarousel(forward) {
@@ -63,6 +64,9 @@
 											$(this).css({
 												left: 0
 											});
+											$scope.$apply(function() {
+												$scope.activeIndex++;
+											})
 										})
 									} else {
 										$ul.find("li:first").before($ul.find("li:last"));
@@ -72,16 +76,50 @@
 										$ul.animate({
 											left: 0
 										}, ($scope.options.slideDuration || 1000), function() {
-
-
+											$scope.$apply(function() {
+												var activeIndex = --$scope.activeIndex;
+												if (activeIndex < 0) {
+													$scope.activeIndex = $scope.images.length - Math.abs(activeIndex);
+												}
+											})
 										})
 									}
+								}
+
+								$scope.$watch('activeIndex', function(n) {
+
+								})
+
+								$scope.navigateThumbPic = function($index) {
+                                    console.log($index - $scope.activeIndex);
+									// if ($index >= $scope.activeIndex) {
+                                    if(true){
+										$ul.animate({
+											left: '-' + $attrs.width * $index + 'px'
+										}, ($scope.options.slideDuration || 1000), function() {
+											$scope.$apply(function() {
+												$scope.activeIndex = $index;
+											})
+										})
+									} else {
+                                        $ul.animate({
+											left: '+' + $attrs.width * ($scope.activeIndex - $index) + 'px'
+										}, ($scope.options.slideDuration || 1000), function() {
+											$scope.$apply(function() {
+												$scope.activeIndex = $index;
+											})
+										})
+									}
+
+								}
+
+								$scope.getActiveClass = function($index) {
+									return $index === $scope.activeIndex;
 								}
 
 								$scope.next = function() {
 									rotateCarousel(true);
 								}
-
 
 								$scope.prev = function() {
 									rotateCarousel(false);
